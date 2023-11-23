5279aecb88b689b78452459930d943a23e855331
elop123 <142408289+elop123@users.noreply.github.com>
2023-11-23 12:40:40 +0100

    input

--- assets/js/site.js
+++ assets/js/site.js
@@ -9,11 +9,10 @@ const mySurnameElement = document.getElementById('surname');
 const myAdressElement = document.getElementById('adress');
 const myPostElement = document.getElementById('post');
 const myEmailElement = document.getElementById('email');
-const myZipCodeElement = document.getElementById('zipCode');
 
 const myButton = document.getElementById('submitButton');
 
-myButton.addEventListener('click', callBack);
+// myButton.addEventListener('click', callBack);
 
 
 
@@ -25,55 +24,39 @@ myButton.addEventListener('click', (event) =>  {
     // trim whitespace fra navn
     let myTrimmedName = myNameElement.value.trim();
 
-<<<<<<< HEAD
     let myTrimmeSurname =mySurnameElement.value.trim();
 
     let myTrimmeAdress =myAdressElement.value.trim();
 
 
-
-=======
-    let myZipCode = myZipCodeElement.value;
+    
 
 
-    if (myTrimmedName.length >= 2 && validateEmail(myEmailElement.value) && OnlyNumber(myZipCode)) {
-        console.log('formen er ydfyldt korrekt');
->>>>>>> 0be4b1e6f61b7656c01df19a02f77c0cfb10da89
+    if (myTrimmedName.length >1 && myTrimmeSurname.length >1 && myTrimmeAdress.length >4 && validatePostNumber(myPostElement.value) && validateEmail(myEmailElement.value))
 
-
-    if (myTrimmedName.length > 2 && myTrimmeSurname.length > 2 && myTrimmeAdress > 5 && validatePostNumber(myPostElement.value) && validateEmail(myEmailElement.value))
     {
         console.log('The form was correctly filled!');
+        alert('The form is filled up!');
         
-
-
-        // formParent.innerText = '<h2>Tak for tilmeldingen!</h2>';
+       formParent.innerHTML = ' ';
 
           let myResponseElement = document.createElement('h2');
-          myResponseElement.innerText = 'tak for tilmeldingen';
+          myResponseElement.innerText = 'Tak for tilmeldingen';
   
          formParent.appendChild(myResponseElement);
 
     } else {
 
         // name evaluation
-<<<<<<< HEAD
         if (myTrimmedName.length  < 2) {
          console.log('Name er IKKE ok');
          alert('Navn skal have min 2 bogstave!');
             
-=======
-        if (myTrimmedName.length < 2) {
-
-            console.log('name er IKKE ok');
-            //alert('name er IKKE ok');
-
->>>>>>> 0be4b1e6f61b7656c01df19a02f77c0cfb10da89
             // inline styling  
             //myNameElement.style = 'border: 2px solid red;'
 
             // tilføj class fra CSS
-            myNameElement.classList.add('errorMarking');
+            myNameElement.classList.toggle('errorMarking');
 
         }
 
@@ -98,34 +81,22 @@ myButton.addEventListener('click', (event) =>  {
         }
         // postnummer evaluation
         if (validatePostNumber(myPostElement.value)){
-
+            // alert('det er ok');
         } else{
             console.log('post nummer er ikke OK')
-            alert("Please enter only numbers!")
             myPostElement.classList.toggle('errorMarking');
         }
 
        
         
         // email validation
-        if (!validateEmail(myEmailElement.value)) {
+        if (validateEmail(myEmailElement.value)) {
 
+        } else {
             console.log('email er IKKE ok');
-<<<<<<< HEAD
             alert('Du skal skrive en rigtig email!');
             // tilføj class fra CSS
         myEmailElement.classList.toggle('errorMarking');
-=======
-
-            myEmailElement.classList.add('errorMarking');
-        }
-
-
-        if (!OnlyNumber(myZipCode)) {
-            console.log('zip er IKKE ok');
-
-            myZipCodeElement.classList.add('errorMarking');
->>>>>>> 0be4b1e6f61b7656c01df19a02f77c0cfb10da89
         }
     }
 });
@@ -135,7 +106,7 @@ myButton.addEventListener('click', (event) =>  {
 
      function validatePostNumber(post){
 
-        let pattern =  /^-?\d*\.?\d+$/;
+        let pattern =  /[0-9]/;
 
         let valid = pattern.test(post);
 
@@ -153,42 +124,16 @@ myButton.addEventListener('click', (event) =>  {
         return isValid;
     }
 
-     function callBack(event) {
-         event.preventDefault();
-         console.log('clicked fra named function');
-         
-         alert('The form needs to be filled up!')
-    }
+    //  function callBack(event) {
+    //      event.preventDefault();
+    //      console.log('clicked fra named function');
+    //     //  alert('The form needs to be filled up!')
+    // }
 
-<<<<<<< HEAD
-    
-=======
-    // Returner resultatet af valideringen (true eller false)
-    return isValid;
-}
+    // function myButton2(){
+    //     document.getElementById('submitButton')
+    //     console.log('clicked');
+    //     alert('The form is completed!');
+    // }
 
-
-
-
-function OnlyNumber(input) {
-    // Brug et regulær udtrykning (regex) for at tjekke om strengen kun indeholder tal
-    var talRegex = /^[0-9]+$/;
-
-    // Test om input strengen opfylder kravet om kun at indeholde tal
-    return talRegex.test(input);
-}
-
-
-
-
-
-
-
-
-
-
-function callBack(event) {
-    event.preventDefault();
-    console.log('clicked fra named function');
-}
->>>>>>> 0be4b1e6f61b7656c01df19a02f77c0cfb10da89
+  
